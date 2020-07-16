2c2f99ebfa6dcaecc0f2991f88adcf08902d6fe4
Chalapathir1 <rchalapaty@gamil.com>
2020-07-16 06:40:49 +0000

    modified helloworld file

--- HelloWorld.java
+++ HelloWorld.java
@@ -32,8 +32,9 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class HelloWorld 
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+      System.out.println("Hello, World");
+	System.out.println("Hello");
   }
 }
